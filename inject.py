377398be215f5c733f47832ab3909c82ef6aac18
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2021-09-28 16:43:43 +0530

    update inject.py

--- inject.py
+++ inject.py
@@ -18,5 +18,5 @@ for k in range(500):
     LOGGING("🚩 JAI SHREE RAM\n"*20)
   if botNo == THREAD_COUNT - 1:
     ATTEMPT += 1
-    if ATTEMPT % 500 == 0:
-      LOGGING(f"#TASK\n```{GROUP_NAME}``` Group have completed {ATTEMPT} attempt with {THREAD_COUNT} Bots")
+    if ATTEMPT % 1000 == 0:
+      LOGGING(f"🧔 #TASK\n#{GROUP_NAME} Group have completed {ATTEMPT} attempt with {THREAD_COUNT} Bots")
