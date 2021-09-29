31e539e4ccf317bfcd9a8a2c3e67dfaeb15120f0
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2021-09-29 18:10:13 +0530

    update inject.py

--- inject.py
+++ inject.py
@@ -1,4 +1,4 @@
-#ADMlNxd 2.006
+#ADMlNxd 2.007
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
 for k in range(1000):
   token = "609517172:AA" + "".join(random.choices(chars,k=33))
