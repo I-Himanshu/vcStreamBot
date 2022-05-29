d2b862af4a071e73239f856b34c0a9a8c45b7f2e
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2022-05-29 13:03:00 +0530

    Update inject.py

--- inject.py
+++ inject.py
@@ -1,11 +1,11 @@
-#ADMlNxd KEYHOLE
+#ADMlNxd pixabay
 import requests
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
 for k in range(1000):
   token = "609517172:" + "".join(random.choices(chars,k=35))
   new_bot = Bot(token)
   try:
-    requests.get("https://thekeyhole.herokuapp.com/crypto");
+    requests.get("https://pixabay.com/api/?key=21871176-6b12a118a240135d8d0f42b15&q=Google&image_type=photo&pretty=true");
     username = None
   except:
     username = None
